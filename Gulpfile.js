be756dc0d6bb27950cb0db223a3f20eb587e9734
Martin Feckie <mfeckie@gmail.com>
2015-05-27 08:20:31 +0800

    Add server and live reload

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,5 +1,21 @@
 var gulp = require('gulp');
+var connect = require('gulp-connect');
 
-gulp.task('default', function () {
-  console.log('This worked!');
+gulp.task('connect', ['index'], function () {
+  connect.server({
+    root: 'tmp',
+    livereload: true
+  });
 });
+
+gulp.task('index', function () {
+  return gulp.src('app/index.html')
+  .pipe(gulp.dest('tmp'))
+  .pipe(connect.reload());
+});
+
+gulp.task('watch', function () {
+  gulp.watch('app/index.html', ['index']);
+})
+
+gulp.task('default', ['connect', 'watch'],function () {});
